e9330f17fde4ba218805f510de41d90348072252
Alexandru Rosianu <rosian_alex@yahoo.com>
2015-07-09 13:09:13 +0300

    Fixes Token model schema

--- models/token.js
+++ models/token.js
@@ -8,7 +8,7 @@ const Token = thinky.createModel('Token', {
   value: type.string().required(),
   accountId: type.string().required(),
   clientId: type.string().required(),
-  scope: type.array().schema(type.number()).required(),
+  scope: type.array().schema(type.string()).required(),
   entity: type.string().enum(entities.FIRST_PARTY, entities.THIRD_PARTY).default(entities.FIRST_PARTY),
   createdAt: type.date().default(r.now())
 }, {
