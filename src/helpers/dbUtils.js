2b9bba5c0b26d067b3a7af1595f0eaaf955356d4
Alexandru Rosianu <rosian_alex@yahoo.com>
2015-07-18 00:47:10 +0300

    More coverage ignore comments

--- src/helpers/dbUtils.js
+++ src/helpers/dbUtils.js
@@ -5,10 +5,12 @@ async function clearDatabase() {
   const tableList = r.tableList();
   const tables = await promisify(tableList.run, tableList)();
 
+  // $lab:coverage:off$
   await* tables.map(table => {
     const del = r.table(table).delete();
     return promisify(del.run, del)();
   });
+  // $lab:coverage:on$
 }
 
 export default {
